fba0f5f65839f257baa1ca630d1b0c5a1a5174c3
popomama <wei_xia@hotmail.com>
2015-04-19 00:31:21 -0400

    scafflding for album

---
+++ Test1/Controllers/StoreController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test1.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public string Index()
+        {
+            return "Hello from Store.Index()";
+            //return View();
+        }
+
+        //public string Browse()
+        //{
+        //    return "Hello from Store.Browse()";
+        //    //return View();
+        //}
+
+        public string Browse(string genre)
+        {
+            return     HttpUtility.HtmlEncode("tore.Browse, Genre = " + genre);
+        }
+
+        // GET: /Store/Details
+        public string Details(int id)
+        {
+            return "Hello from Store.Details() " + id;
+        }
+    }
+}
